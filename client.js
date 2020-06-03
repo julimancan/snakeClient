e34c48d188c8ffc148fc3c06fa777abfeee18486
Julian Bustos <julian.m.bustos@gmail.com>
2020-06-03 23:18:45 +0000

    added client file and connected succesfully

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function (data) {
+  const conn = net.createConnection({
+    host: '135.23.222.148',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.on('data', data => {
+    console.log(data);
+  })
+  conn.on('connect', message => {
+    console.log("Connection established begin time wasting activity now!")
+  })
+  conn.on('connect', () => {
+    conn.write('Name: JUL');
+  });
+  conn.setEncoding('utf8');
+  return conn;
+}
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,9 @@
-const net = require('net');
+
 
 /**
  * Establishes connection with the game server
  */
-const connect = function (data) {
-  const conn = net.createConnection({
-    host: '135.23.222.148',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.on('data', data => {
-    console.log(data);
-  })
-  conn.setEncoding('utf8');
-  return conn;
-}
-
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
-
-module.exports = { connect };
